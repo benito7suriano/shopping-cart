2be6496b1aea39a315ab071dbc2c228ac932e25b
Beno Suriano <bjsuriano@gmail.com>
2022-09-14 12:25:11 -0400

    Create navbar with logo and searchbar.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Container, FormControl, Navbar } from 'react-bootstrap'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <Navbar bg='dark' variant='dark' style={{ height: 80 }}>
+      <Container>
+        <Navbar.Brand>
+          <a href=''>Shopping Cart</a>
+        </Navbar.Brand>
+        <Navbar.Text className='search'>
+          <FormControl
+            style={{ width: 500 }}
+            placeholder='search a product'
+            className='m-auto'
+          />
+        </Navbar.Text>
+      </Container>
+    </Navbar>
+  )
 }
 
 export default Header
