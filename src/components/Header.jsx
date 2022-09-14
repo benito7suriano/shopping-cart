7f319ea5331d401c27400abb1dab9f4efa9706a8
Beno Suriano <bjsuriano@gmail.com>
2022-09-14 12:34:22 -0400

    Complete navbar.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
-import { Container, FormControl, Navbar } from 'react-bootstrap'
+import {
+  Container,
+  FormControl,
+  Navbar,
+  Nav,
+  Dropdown,
+  Badge,
+} from 'react-bootstrap'
 
-const Header = () => {
+import { FaShoppingCart } from 'react-icons/fa'
+
+const Header = ({ cart }) => {
   return (
     <Navbar bg='dark' variant='dark' style={{ height: 80 }}>
       <Container>
@@ -13,8 +22,21 @@ const Header = () => {
             style={{ width: 500 }}
             placeholder='search a product'
             className='m-auto'
+            aria-label='Search'
           />
         </Navbar.Text>
+        <Nav>
+          <Dropdown alignRight>
+            <Dropdown.Toggle variant='success'>
+              <FaShoppingCart color='white' fontSize={25} />
+              <Badge>{10}</Badge>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu style={{ minWidth: 370 }}>
+              <span style={{ padding: 10 }}>Cart is empty</span>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
       </Container>
     </Navbar>
   )
