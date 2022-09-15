a3b822474efb9def498377a2b53ead08c086edb0
Beno Suriano <bjsuriano@gmail.com>
2022-09-15 08:45:52 -0400

    Create faker products.

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,9 +1,22 @@
 import React, { createContext } from 'react'
+import { faker } from '@faker-js/faker'
 
 const Cart = createContext()
 
 const Context = ({ children }) => {
-  return <Cart.Provider>{children}</Cart.Provider>
+  const products = [...Array(20)].map(() => ({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.image.abstract(),
+    inStock: faker.random.numeric(),
+    fastDelivery: faker.datatype.boolean(),
+    ratings: Math.ceil(Math.random() * 5),
+  }))
+
+  console.log(products)
+
+  return <Cart.Provider value={products}>{children}</Cart.Provider>
 }
 
 export default Context
