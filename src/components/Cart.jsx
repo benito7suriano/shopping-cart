c73df0d50be081056124271f2e0b6aa729958b93
Beno Suriano <bjsuriano@gmail.com>
2022-09-19 20:00:42 -0400

    Render product name on cart page.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { CartState } from '../context/Context'
+
+import { ListGroup } from 'react-bootstrap'
 
 const Cart = () => {
-  return <div>Cart</div>
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState()
+
+  return (
+    <div className='home'>
+      <div className='productContainer'>
+        <ListGroup>
+          {cart.map((prod) => (
+            <span>{prod.name}</span>
+          ))}
+        </ListGroup>
+      </div>
+      <div className='filters summary'></div>
+    </div>
+  )
 }
 
 export default Cart
