8d588afb0b5a900a8fac02720006940841bac477
Beno Suriano <bjsuriano@gmail.com>
2022-09-20 09:58:10 -0400

    Implement sort and out of stock and fast delivery filters on home page.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,13 +8,43 @@ import './styles.css'
 const Home = () => {
   const {
     state: { products },
+    productState: { sort, byStock, byFastDelivery, byRating, searchQuery },
   } = CartState()
+
+  const transformProducts = () => {
+    let sortedProducts = products
+    if (sort) {
+      sortedProducts = sortedProducts.sort((a, b) =>
+        sort === 'lowToHigh' ? a.price - b.price : b.price - a.price,
+      )
+    }
+
+    if (!byStock) {
+      sortedProducts = sortedProducts.filter((prod) => prod.inStock)
+    }
+
+    if (byFastDelivery) {
+      sortedProducts = sortedProducts.filter((prod) => prod.fastDelivery)
+    }
+
+    if (byRating) {
+      sortedProducts = sortedProducts.filter((prod) => prod.ratings >= byRating)
+    }
+
+    if (searchQuery) {
+      sortedProducts = sortedProducts.filter((prod) =>
+        prod.name.toLowerCase().includes(searchQuery),
+      )
+    }
+
+    return sortedProducts
+  }
 
   return (
     <div className='home'>
       <Filters />
       <div className='productContainer'>
-        {products.map((product) => {
+        {transformProducts().map((product) => {
           return <SingleProduct key={product.id} product={product} />
         })}
       </div>
